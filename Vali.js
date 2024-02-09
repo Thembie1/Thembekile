0cc0b20bb582ad678e36396815dfb14bdf770db7
Thembekile <138671990+Thembie1@users.noreply.github.com>
2024-02-09 14:17:23 +0200

    Update Vali.js

--- Vali.js
+++ Vali.js
@@ -1,6 +1,6 @@
 function redirectToPage(answer) {
     if (answer === "yes") {
-        window.location.href = "Yes.html";
+        window.location.href = "Yes.html ";
     } else {
         window.location.href = "no.html";
     }
